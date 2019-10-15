a89b4aef85e61302b0b202dc5b004cec0afe1729
jjlmoya <juanjo1989@gmail.com>
2019-10-15 19:58:32 +0200

    Initial commit game sample

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import Game from "/src/game";
+
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const GAME_WIDTH = canvas.width;
+const GAME_HEIGHT = canvas.height;
+
+let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+window.game = game;
+let lastTime = 0;
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+  game.update(deltaTime);
+  game.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
